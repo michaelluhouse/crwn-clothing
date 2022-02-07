6d5a6d6e9de06174a9cd3fdfc9fef36dca06cdf3
The Human <thehuman@Michael.local>
2022-02-07 13:38:45 +0800

    wrote about firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth';
+
+// const config = {
+//     apiKey: "AIzaSyCQwA0RPnOzZ4cR5hVhqQ637qRvtR93qBI",
+//     authDomain: "crwn-db-f14bc.firebaseapp.com",
+//     projectId: "crwn-db-f14bc",
+//     storageBucket: "crwn-db-f14bc.appspot.com",
+//     messagingSenderId: "613274123057",
+//     appId: "1:613274123057:web:410558fdda85b2bdbd4e10",
+//     measurementId: "G-GMJKFC9M11"
+// };
+
+// firebase.initializeApp(config);
+
+// export const auth = firebase.auth();
+// export const firestore = firebase.firestore();
+
+// const provider = new firebase.auth.GoogleAuthProvider();
+// provider.setCustomParameters({ prompt: 'select_account' });
+// export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+// export default firebase;
+
